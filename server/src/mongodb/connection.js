1753ed376418299ec836f706fa2f26f644c4bdcd
Jacob Bare <jacob@parameter1.com>
2021-05-28 09:09:18 -0500

    Enable auto indexing of mongodb when not in prod

--- server/src/mongodb/connection.js
+++ server/src/mongodb/connection.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
-const { MONGO_DSN } = require('../env');
+const { MONGO_DSN, isProduction } = require('../env');
 const { name, version } = require('../../package.json');
 
 module.exports = mongoose.createConnection(MONGO_DSN, {
   appname: `${name} v${version}`,
+  autoIndex: !isProduction,
   bufferMaxEntries: 0, // Default -1
   connectTimeoutMS: 200, // TCP Connection timeout setting (default 30000)
   ignoreUndefined: true,
