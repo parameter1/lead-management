a2aa25b09a6658504c1480144a424566ab898587
Jacob Bare <jacob@parameter1.com>
2021-06-01 11:12:47 -0500

    Add parameter checks to entity utils

--- server/src/omeda/utils/customer-entity.js
+++ server/src/omeda/utils/customer-entity.js
@@ -4,10 +4,11 @@ const createHash = require('../../utils/create-hash');
 
 module.exports = ({ customerId, emailAddress } = {}) => {
   if (!customerId && !emailAddress) throw new Error('A customer ID or email address is required.');
+  if (customerId && emailAddress) throw new Error('You cannot provide both a customer ID and email address');
   return entityId({
     brand: omeda.brand,
     type: 'customer',
-    id: `${customerId}` || createHash(emailAddress),
+    id: customerId ? `${customerId}` : createHash(emailAddress.toLowerCase().trim()),
     idType: customerId ? undefined : 'hashed-email',
   });
 };
