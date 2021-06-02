ca93e585191b29dacb86bda5dfc63bc894d9a993
Jacob Bare <jacob@parameter1.com>
2021-06-02 15:50:49 -0500

    Add fields to email deployment url schema

--- server/src/mongodb/schema/omeda/email-deployment-url.js
+++ server/src/mongodb/schema/omeda/email-deployment-url.js
@@ -1,7 +1,26 @@
 const { Schema } = require('mongoose');
 
-// this model is for indexing only.
-const schema = new Schema({});
+const deploymentSchema = new Schema({
+  entity: { type: String },
+  name: { type: String },
+  designation: { type: String },
+  sentDate: { type: Date },
+  typeId: { type: Number },
+}, { _id: false });
+
+const hostSchema = new Schema({
+  value: { type: String },
+});
+
+const schema = new Schema({
+  deployment: { type: deploymentSchema },
+  urlId: { type: Schema.Types.ObjectId },
+  host: { type: hostSchema },
+  customerId: { type: Schema.Types.ObjectId },
+  linkType: { type: String },
+  omeda: { type: Schema.Types.Mixed },
+  tagIds: { type: [Schema.Types.ObjectId] },
+});
 
 schema.index({ urlId: 1, 'deployment.entity': 1 }, { unique: true });
 schema.index({ 'deployment.entity': 1 });
