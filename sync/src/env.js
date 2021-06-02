7d18e35e04e9c5e4b9e19118a03fbbdd2adce88f
Jacob Bare <jacob@parameter1.com>
2021-06-02 09:22:10 -0500

    Add `AWS_EXECUTION_ENV` env val

--- sync/src/env.js
+++ sync/src/env.js
@@ -4,6 +4,7 @@ const {
 } = require('envalid');
 
 module.exports = cleanEnv(process.env, {
+  AWS_EXECUTION_ENV: str({ desc: 'This will be provided by the lambda runtime.', default: undefined }),
   LEGACY_MONGO_DSN: str({ desc: 'The legacy "leads-graph" MongoDB DSN to connect to.' }),
   MONGO_DSN: str({ desc: 'The MongoDB DSN to connect to.' }),
   OMEDA_APP_ID: str({ desc: 'The Omeda API App ID.' }),
--- sync/src/lambda/deployments.js
+++ sync/src/lambda/deployments.js
@@ -3,10 +3,13 @@ const loadDB = require('../mongodb/load-db');
 const dayjs = require('../dayjs');
 const loadIds = require('../ops/load-deployment-ids-since');
 const upsert = require('../commands/upsert-deployments');
+const { AWS_EXECUTION_ENV } = require('../env');
 
 const { log } = console;
 
-exports.handler = async () => {
+exports.handler = async (event, context = {}) => {
+  // see https://docs.atlas.mongodb.com/best-practices-connecting-to-aws-lambda/
+  context.callbackWaitsForEmptyEventLoop = false;
   const db = await loadDB();
 
   // get the last omeda deployment saved in the db
@@ -21,6 +24,6 @@ exports.handler = async () => {
   const trackIds = await loadIds({ onOrAfter: start });
   log(`Found ${trackIds.length} deployment(s) to upsert.`, trackIds);
   if (trackIds.length) await upsert({ trackIds });
-  await mongodb.close();
+  if (!AWS_EXECUTION_ENV) await mongodb.close();
   log('DONE');
 };
