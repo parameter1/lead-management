dd55c59e111a9b478f594d8ffdad9bd7b9eee98f
Jacob Bare <jacob@parameter1.com>
2021-06-02 07:57:49 -0500

    Change `after` param to `onOrAfter`

--- sync/src/ops/load-deployment-ids-since.js
+++ sync/src/ops/load-deployment-ids-since.js
@@ -7,17 +7,17 @@ const dayjs = require('../dayjs');
  * Gets all sent or sending deployment IDs since/after a certain date.
  *
  * @param {object} params
- * @param {Date} params.date
+ * @param {Date} params.onOrAfter
  * @param {number} [params.clockDriftMinutes=60]
  * @returns {string[]}
  */
 module.exports = async (params = {}) => {
-  const { after, clockDriftMinutes } = await validateAsync(Joi.object({
-    after: Joi.date().required(),
+  const { onOrAfter, clockDriftMinutes } = await validateAsync(Joi.object({
+    onOrAfter: Joi.date().required(),
     clockDriftMinutes: Joi.number().min(0).default(60),
   }), params);
 
-  const start = dayjs(after).subtract(clockDriftMinutes, 'minutes').toDate();
+  const start = dayjs(onOrAfter).subtract(clockDriftMinutes, 'minutes').toDate();
   const { data } = await omeda.resource('email').searchDeployments({
     deploymentDateStart: start,
     numResults: 1000,
