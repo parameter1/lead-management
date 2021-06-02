377501c9044505586495c22d4030323bc98dbed3
Jacob Bare <jacob@parameter1.com>
2021-06-02 11:36:08 -0500

    Create process identity records lambda

---
+++ sync/test/lambda/process-identity-records.js
@@ -0,0 +1,8 @@
+const { handler } = require('../../src/lambda/process-identity-records');
+
+handler({
+  Records: [
+    { body: '{"EmailAddress":"brandon@parameter1.com","FirstName":"Brandon","LastName":"Krigbaum"}' },
+    { body: '{"EmailAddress":"jacob@parameter1.com","FirstName":"Jacob","LastName":"Bare"}' },
+  ],
+}).catch((e) => setImmediate(() => { throw e; }));
