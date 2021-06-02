b00333b5832e1257b840f61b159d7dcb72bc08ce
Jacob Bare <jacob@parameter1.com>
2021-06-02 08:29:22 -0500

    Fix date input in deployment IDs since test

--- sync/test/ops/load-deployment-ids-since.js
+++ sync/test/ops/load-deployment-ids-since.js
@@ -2,5 +2,5 @@ const run = require('../_run');
 const dayjs = require('../../src/dayjs');
 const command = require('../../src/ops/load-deployment-ids-since');
 
-run(command, { after: dayjs().subtract(2, 'weeks').toDate() })
+run(command, { onOrAfter: dayjs().subtract(2, 'weeks').toDate() })
   .catch((e) => setImmediate(() => { throw e; }));
