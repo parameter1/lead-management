e8c5d4e2f66b156164aa7094b8a182ca954b0b76
Jacob Bare <jacob@parameter1.com>
2021-06-02 11:37:53 -0500

    Set indm tenant for migration dsn

--- migrate/src/env.js
+++ migrate/src/env.js
@@ -5,7 +5,7 @@ const {
 } = require('envalid');
 
 module.exports = cleanEnv(process.env, {
-  DESTINATION_DB_NAME: str({ desc: 'The database to write data to.', default: 'lead-management' }),
+  DESTINATION_DB_NAME: str({ desc: 'The database to write data to.', default: 'lead-management-indm' }),
   DESTINATION_MONGO_URI: str({ desc: 'The MongoDB instance to write data to.' }),
   SOURCE_DB_NAME: str({ desc: 'The database to read data from.', default: 'leads-graph' }),
   SOURCE_MONGO_URI: str({ desc: 'The MongoDB instance to read data from.' }),
