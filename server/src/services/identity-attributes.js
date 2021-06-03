725ff67381920d7a6b6a51413af41b8231123c6e
Jacob Bare <jacob@parameter1.com>
2021-06-03 15:48:57 -0500

    Create identity attributes service; disable deep attrs

---
+++ server/src/services/identity-attributes.js
@@ -0,0 +1,16 @@
+module.exports = [
+  { key: 'emailAddress', label: 'Email' },
+  { key: 'givenName', label: 'First Name' },
+  { key: 'familyName', label: 'Last Name' },
+  { key: 'title', label: 'Title' },
+  { key: 'companyName', label: 'Company Name' },
+  { key: 'street', label: 'Address' },
+  { key: 'city', label: 'City' },
+  { key: 'region', label: 'State' },
+  { key: 'postalCode', label: 'Postal Code' },
+  { key: 'country', label: 'Country' },
+  { key: 'phoneNumber', label: 'Phone #', adminOnly: true },
+  // { key: 'attributes.Industry', label: 'Industry' },
+  // { key: 'attributes.Job Function', label: 'Job Function' },
+  // { key: 'attributes.NAICS Code', label: 'NAICS' },
+];
