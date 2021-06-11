876d72a1e7b5a035f298459f0c81f0a58c4d1d5a
Jacob Bare <jacob@parameter1.com>
2021-06-11 08:22:24 -0500

    Move export route to root

--- server/src/routes/exports/index.js
+++ server/src/routes/exports/index.js
@@ -1,11 +1,22 @@
 const { Router } = require('express');
 const noCache = require('nocache');
 
-const campaignEmailMetrics = require('./campaign-email-metrics');
+const asyncRoute = require('../../utils/async-route');
+const micro = require('../../micro');
+const Campaign = require('../../mongodb/models/campaign');
 
 const router = Router();
 router.use(noCache());
 
-campaignEmailMetrics(router);
+router.get('/campaign/:hash/email/metrics', asyncRoute(async (req, res) => {
+  const { hash } = req.params;
+  const campaign = await Campaign.findByHash(hash);
+
+  const csv = await micro.exports.request('campaign.emailMetrics', { hash });
+  const filename = `${campaign.fullName} Email Metrics.csv`;
+  res.header('content-type', 'text/csv');
+  res.attachment(filename);
+  res.send(csv);
+}));
 
 module.exports = router;
