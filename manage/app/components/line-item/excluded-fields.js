8aa1910defe79eb23f89ec891c897086aac74b23
Jacob Bare <jacob@parameter1.com>
2021-06-16 09:27:37 -0500

    Disable excluded fields when editorial is selected

--- manage/app/components/line-item/excluded-fields.js
+++ manage/app/components/line-item/excluded-fields.js
@@ -2,6 +2,8 @@ import Component from '@ember/component';
 import { inject } from '@ember/service';
 import { computed } from '@ember/object';
 
+const { isArray } = Array;
+
 export default Component.extend({
   classNames: ['form-group'],
 
@@ -16,6 +18,19 @@ export default Component.extend({
     const selected = this.get('excludedFields');
     return this.get('identityAttributes.getViewableFields').filter(o => selected.includes(o.key));
   }),
+
+  isEditorial: computed('tags.@each.name', 'linkTypes.[]', function() {
+    const pr = this.get('tags').find(tag => tag.name === 'PR');
+    const editorialLink = this.get('linkTypes').includes('Editorial');
+    if (pr || editorialLink) return true;
+    return false;
+  }),
+
+  init() {
+    this._super(...arguments);
+    if (!isArray(this.get('tags'))) this.set('tags', []);
+    if (!isArray(this.get('linkTypes'))) this.set('linkTypes', []);
+  },
 
   actions: {
     onChange(fields) {
