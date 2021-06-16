b42e1b8c2d2548d26a8e37d3bbd9c6ff891d3136
Jacob Bare <jacob@parameter1.com>
2021-06-16 12:35:25 -0500

    Fix semicolon

--- monorepo/packages/lambda/env.js
+++ monorepo/packages/lambda/env.js
@@ -11,5 +11,5 @@ module.exports = cleanEnv(process.env, {
   AWS_REGION: str({ desc: 'The AWS region. This will be provided by the lambda runtime.', default: 'us-east-2' }),
   AWS_SECRET_ACCESS_KEY: str({ desc: 'This will be provided by the lambda runtime.' }),
 
-  TENANT_KEY: str({ desc: 'The lead management tenant key' });
+  TENANT_KEY: str({ desc: 'The lead management tenant key' }),
 });
