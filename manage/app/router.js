167a2b60a810b5cdbaf45334d3d4023e84183ad5
Jacob Bare <jacob@parameter1.com>
2021-09-15 10:21:38 -0500

    Rename campaign-reporting to campaign-metrics

--- manage/app/router.js
+++ manage/app/router.js
@@ -148,7 +148,7 @@ Router.map(function() {
     });
     this.route('reporting');
     this.route('click-events');
-    this.route('campaign-reporting');
+    this.route('campaign-metrics');
   });
   this.route('video', function() {
     this.route('edit', { path: ':id' });
