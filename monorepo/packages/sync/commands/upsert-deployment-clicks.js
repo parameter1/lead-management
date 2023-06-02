abe8c56db71b9427ec77af0c30e354ce8bdaff03
Jacob Bare <jacob@parameter1.com>
2023-06-02 10:47:58 -0500

    Include `invalid` clicks on deployment click upserts

--- monorepo/packages/sync/commands/upsert-deployment-clicks.js
+++ monorepo/packages/sync/commands/upsert-deployment-clicks.js
@@ -8,6 +8,70 @@ const loadDeploymentClicks = require('../ops/load-deployment-clicks');
 const extractUrlId = require('../utils/extract-url-id');
 
 /**
+ * @typedef {string} OmedaClickKey
+ *
+ * @typedef OmailClick
+ * @prop {string} [EncryptedCustomerId]
+ * @prop {Date} ClickDate
+ * @prop {string} [EmailAddress]
+ * @prop {string} [FirstName]
+ * @prop {string} [LastName]
+ * @prop {number} NumberOfClicks
+ *
+ * @typedef OmailUnrealClick
+ * @prop {string} [EncryptedCustomerId]
+ * @prop {string} [EmailAddress]
+ * @prop {string} [FirstName]
+ * @prop {string} [LastName]
+ * @prop {OmailUnrealClickDetail[]} UnrealClicks
+ *
+ * @todo MAKE SURE THIS RETURNS FROM OUR API CLIENT PROPERLY
+ * @typedef OmailUnrealClickDetail
+ * @prop {number} NumberOfUnrealClicks
+ * @prop {Date} ClickDate
+ * @prop {OmailUnrealClickReason} Reason
+ *
+ * @typedef {(1|2|3|4|5|6|7|8|9|10)} OmailUnrealClickReason
+ *
+ * @typedef MappedClick
+ * @prop {UpsertClickFilter} filter
+ * @prop {Date} date
+ * @prop {number} n
+ * @prop {Map<OmailUnrealClickReason, InvalidMappedClick>} invalid
+ *
+ * @typedef InvalidMappedClick
+ * @prop {Date} date
+ * @prop {number} n
+ *
+ * @typedef UpsertClickFilter
+ * @prop {import("mongodb").ObjectId} url
+ * @prop {string} idt
+ * @prop {string} dep
+ * @prop {string} split
+ */
+
+/**
+ * @param {MappedClick} mapped
+ */
+const createUpsertOpFrom = (mapped) => {
+  const { filter, invalid } = mapped;
+  return {
+    updateOne: {
+      filter,
+      update: {
+        $setOnInsert: filter,
+        $set: {
+          date: mapped.date,
+          n: mapped.n,
+          invalid: [...invalid].map(([code, click]) => ({ code, ...click })),
+        },
+      },
+      upsert: true,
+    },
+  };
+};
+
+/**
  * Upserts deployment click event data for the provided deployment IDs.
  * Also returns the encrypted customer IDs and customer records associated with the click events
  * that can be used for customer upserting.
@@ -26,6 +90,22 @@ module.exports = async (params = {}) => {
 
   const items = await loadDeploymentClicks({ trackIds }, tenant);
 
+  /**
+   *
+   * @param {OmailClick|OmailUnrealClick} click
+   * @returns {string}
+   */
+  const getIdentityFrom = (click) => {
+    const {
+      EncryptedCustomerId: encryptedCustomerId,
+      EmailAddress: emailAddress,
+    } = click;
+
+    return encryptedCustomerId
+      ? omeda.entity.customer({ encryptedCustomerId })
+      : omeda.entity.customer({ emailAddress });
+  };
+
   const eventOps = [];
   const encryptedCustomerIds = new Set();
   const identityRecords = new Map();
@@ -34,34 +114,92 @@ module.exports = async (params = {}) => {
       getAsArray(split, 'links').forEach((link) => {
         const urlId = extractUrlId(link.LinkURL);
         if (!urlId) return;
-        getAsArray(link, 'clicks').forEach((click) => {
+
+        /** @type {Map<OmedaClickKey, MappedClick>} */
+        const clickMap = new Map();
+
+        /**
+         *
+         * @param {OmailClick|OmailUnrealClick} click
+         * @returns {UpsertClickFilter}
+         */
+        const getUpsertFilter = (click) => ({
+          url: new ObjectId(urlId),
+          idt: getIdentityFrom(click),
+          dep: entity,
+          split: split.Split,
+        });
+
+        /**
+         *
+         * @param {OmailClick|OmailUnrealClick} click
+         * @returns {OmedaClickKey}
+         */
+        const getClickKey = (click) => {
+          const filter = getUpsertFilter(click);
+          return Object.keys(filter).reduce((arr, key) => {
+            arr.push(`${filter[key]}`);
+            return arr;
+          }, []).join('__');
+        };
+
+        /**
+         *
+         * @param {OmailClick|OmailUnrealClick} click
+         */
+        const addCustomer = (click) => {
           const {
             EncryptedCustomerId: encryptedCustomerId,
-            ClickDate,
             EmailAddress: emailAddress,
             FirstName,
             LastName,
           } = click;
-          const idt = encryptedCustomerId
-            ? omeda.entity.customer({ encryptedCustomerId })
-            : omeda.entity.customer({ emailAddress });
+          const idt = getIdentityFrom(click);
 
           if (encryptedCustomerId) {
             encryptedCustomerIds.add(encryptedCustomerId);
           } else {
             identityRecords.set(idt, { EmailAddress: emailAddress, FirstName, LastName });
           }
-          const filter = {
-            url: new ObjectId(urlId),
-            idt,
-            dep: entity,
-            split: split.Split,
-          };
-          const update = {
-            $setOnInsert: filter,
-            $set: { date: ClickDate, n: click.NumberOfClicks },
-          };
-          eventOps.push({ updateOne: { filter, update, upsert: true } });
+        };
+
+        /** @type {OmailClick[]} */
+        const clicks = getAsArray(link, 'clicks');
+        clicks.forEach((click) => {
+          addCustomer(click);
+
+          /** @type {MappedClick} */
+          const mapped = clickMap.get(getClickKey(click)) || {};
+          mapped.filter = getUpsertFilter(click);
+          mapped.date = click.ClickDate;
+          mapped.n = click.NumberOfClicks;
+        });
+
+        /** @type {OmailUnrealClick[]} */
+        const unrealClicks = getAsArray(link, 'unrealClicks');
+        unrealClicks.forEach((click) => {
+          addCustomer(click);
+
+          /** @type {MappedClick} */
+          const mapped = clickMap.get(getClickKey(click)) || {};
+          mapped.filter = getUpsertFilter(click);
+          if (!mapped.n) mapped.n = 0;
+          if (!mapped.date) {
+            const [mostRecent] = click.UnrealClicks.sort((a, b) => b.ClickDate - a.ClickDate);
+            mapped.date = mostRecent.ClickDate;
+          }
+          const invalid = mapped.invalid || new Map();
+          click.UnrealClicks.forEach((detail) => {
+            const { Reason: code } = detail;
+            invalid.set(code, {
+              date: detail.ClickDate,
+              n: detail.NumberOfUnrealClicks,
+            });
+          });
+        });
+
+        clickMap.forEach((mapped) => {
+          eventOps.push(createUpsertOpFrom(mapped));
         });
       });
     });
