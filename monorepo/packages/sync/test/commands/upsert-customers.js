8f06f7a635a0864067e23b2b97867b2474d90031
Shinsina <shinsina@att.net>
2023-10-24 11:00:56 -0500

    Return multiTenantRun result

--- monorepo/packages/sync/test/commands/upsert-customers.js
+++ monorepo/packages/sync/test/commands/upsert-customers.js
@@ -2,7 +2,7 @@ const run = require('../_run');
 const command = require('../../commands/upsert-customers');
 
 async function multiTenantRun() {
-  await Promise.all([
+  return Promise.all([
     command({ tenantKey: 'indm', encryptedCustomerIds: ['8797H3317467C8C', '5689J2201356C7J'] }),
     command({ tenantKey: 'lynchm', encryptedCustomerIds: ['4357B1923823E9X'] }),
   ]);
