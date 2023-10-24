4c59fda015fd5c60fbff4fe44d53e09327604647
Shinsina <shinsina@att.net>
2023-10-24 10:59:29 -0500

    Update tests to confirm lynchm demographic sync

--- monorepo/packages/lambda/test/process-customers.js
+++ monorepo/packages/lambda/test/process-customers.js
@@ -5,5 +5,6 @@ handler({
     { body: JSON.stringify({ tenantKey: 'indm', encryptedCustomerId: '2804I4421578C9G' }) },
     { body: JSON.stringify({ tenantKey: 'indm', encryptedCustomerId: '2804I4421578C9G' }) },
     { body: JSON.stringify({ tenantKey: 'indm', encryptedCustomerId: '8797H3317467C8C' }) },
+    { body: JSON.stringify({ tenantKey: 'lynchm', encryptedCustomerId: '4357B1923823E9X' }) },
   ],
 }).catch((e) => setImmediate(() => { throw e; }));
--- monorepo/packages/sync/test/commands/upsert-customers.js
+++ monorepo/packages/sync/test/commands/upsert-customers.js
@@ -1,5 +1,11 @@
 const run = require('../_run');
 const command = require('../../commands/upsert-customers');
 
-run(command, { tenantKey: 'indm', encryptedCustomerIds: ['8797H3317467C8C', '5689J2201356C7J'] })
-  .catch((e) => setImmediate(() => { throw e; }));
+async function multiTenantRun() {
+  await Promise.all([
+    command({ tenantKey: 'indm', encryptedCustomerIds: ['8797H3317467C8C', '5689J2201356C7J'] }),
+    command({ tenantKey: 'lynchm', encryptedCustomerIds: ['4357B1923823E9X'] }),
+  ]);
+}
+
+run(multiTenantRun).catch((e) => setImmediate(() => { throw e; }));
