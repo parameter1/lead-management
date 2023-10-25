e651fd77539ecc6a78acae956161a04dfab92f6e
Shinsina <shinsina@att.net>
2023-10-25 12:51:29 -0500

    Clean up test for upsert-customers command

--- monorepo/packages/sync/test/commands/upsert-customers.js
+++ monorepo/packages/sync/test/commands/upsert-customers.js
@@ -1,11 +1,7 @@
 const run = require('../_run');
 const command = require('../../commands/upsert-customers');
 
-async function multiTenantRun() {
-  return Promise.all([
-    command({ tenantKey: 'indm', encryptedCustomerIds: ['8797H3317467C8C', '5689J2201356C7J'] }),
-    command({ tenantKey: 'lynchm', encryptedCustomerIds: ['4357B1923823E9X'] }),
-  ]);
-}
-
-run(multiTenantRun).catch((e) => setImmediate(() => { throw e; }));
+run(() => Promise.all([
+  command({ tenantKey: 'indm', encryptedCustomerIds: ['8797H3317467C8C', '5689J2201356C7J'] }),
+  command({ tenantKey: 'lynchm', encryptedCustomerIds: ['4357B1923823E9X'] }),
+])).catch((e) => setImmediate(() => { throw e; }));
