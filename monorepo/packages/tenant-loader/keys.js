b2d0524a496fb08713bf14f7f2386a6b3355812f
Jacob Bare <jacob@parameter1.com>
2024-10-07 15:04:39 -0500

    Add support for disabling tenants

--- monorepo/packages/tenant-loader/keys.js
+++ monorepo/packages/tenant-loader/keys.js
@@ -2,5 +2,5 @@ const mongodb = require('@lead-management/mongodb/client');
 
 module.exports = async () => {
   const db = await mongodb.db({ name: 'lead-management' });
-  return db.collection('tenants').distinct('zone');
+  return db.collection('tenants').distinct('zone', { disabled: { $ne: true } });
 };
