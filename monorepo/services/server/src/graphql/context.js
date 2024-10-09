5e47c25d77952e13a8069d7bcc98430693db5d60
Jacob Bare <jacob@parameter1.com>
2024-10-09 10:24:47 -0500

    Support reading custom report config for graphql query string

--- monorepo/services/server/src/graphql/context.js
+++ monorepo/services/server/src/graphql/context.js
@@ -6,7 +6,22 @@ const gam = require('./schema/gam/executor');
 
 module.exports = async ({ req }) => {
   const tenant = await loadTenant({ key: TENANT_KEY });
+  const { query } = req;
+
+  let customClickFilter;
+  if (query.customClickFilter) {
+    const secondsSinceSentTime = Object.keys(query).filter((key) => {
+      if (!/\d+/.test(key)) return false;
+      return parseInt(key, 10) >= 0;
+    }).reduce((o, key) => ({
+      ...o,
+      [key]: { allowUnrealCodes: query[key].split(',').filter((v) => v) },
+    }), {});
+    customClickFilter = { secondsSinceSentTime };
+  }
+
   return {
+    ...(customClickFilter && { customClickFilter }),
     tenant,
     auth: req.auth,
     host: HOST_NAME,
