78ddad8b4c54b24d0b620c7b54d16442d0e33238
Jacob Bare <jacob@parameter1.com>
2024-10-10 10:47:41 -0500

    display click rules in minutes; clean up labels

--- manage/app/components/click-filter/rule.js
+++ manage/app/components/click-filter/rule.js
@@ -1,6 +1,16 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  minutes: computed('model.seconds', {
+    get() {
+      return (this.get('model.seconds') || 0) / 60;
+    },
+    set(_, value) {
+      this.set('model.seconds', value * 60);
+    },
+  }),
+
   actions: {
     setAllowedCodes(codes) {
       this.set('model.allowUnrealCodes', [...codes].sort((a, b) => a - b));
